a35a41d67df5696bb5fdf21014434a81d5f6bb9f
danielstirnemann <stirnemann.daniel@hotmail.com>
2023-06-07 12:34:41 -0400

    add function en to fr

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,1 +1,9 @@
 from deep_translator import MyMemoryTranslator
+
+
+def englishToFrench(englishText):
+    frenchText = MyMemoryTranslator(source='english', target='french').translate(englishText)
+    return frenchText
+
+
+print(englishToFrench("hello"))
