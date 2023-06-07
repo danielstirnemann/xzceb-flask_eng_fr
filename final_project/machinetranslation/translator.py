fd806e7d787c550882ba819ef5d7d9176a6d5c17
danielstirnemann <stirnemann.daniel@hotmail.com>
2023-06-07 13:02:21 -0400

    Changed code according to pylint

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,14 +1,18 @@
+"""This Module translates Text"""
+
 from deep_translator import MyMemoryTranslator
 
 
-def englishToFrench(englishText):
-    frenchText = MyMemoryTranslator(source='english', target='french').translate(englishText)
-    return frenchText
+def englishToFrench(english_text):
+    """This Method translates from english to french"""
+    french_text = MyMemoryTranslator(source='english', target='french').translate(english_text)
+    return french_text
 
 
-def frenchToEnglish(frenchText):
-    englishText = MyMemoryTranslator(source='french', target='english').translate(frenchText)
-    return englishText
+def frenchToEnglish(french_text):
+    """This Method translates from french to english"""
+    english_text = MyMemoryTranslator(source='french', target='english').translate(french_text)
+    return english_text
 
 
 print(englishToFrench("hello"))
